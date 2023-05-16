ea6b17fbc3d821e3ebfe9f312c3a756ad6c3d08b
Minzaker <krzysbrzezinski3@gmail.com>
2023-05-16 11:20:36 +0000

    Whole XO added

---
+++ Form1.Designer.cs
@@ -0,0 +1,219 @@
+﻿namespace XO
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.MasterBtn = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(17, 69);
+            this.button1.Margin = new System.Windows.Forms.Padding(8);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(49, 42);
+            this.button1.TabIndex = 0;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.XorO);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(82, 69);
+            this.button2.Margin = new System.Windows.Forms.Padding(8);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(49, 42);
+            this.button2.TabIndex = 1;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.XorO);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(147, 69);
+            this.button3.Margin = new System.Windows.Forms.Padding(8);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(49, 42);
+            this.button3.TabIndex = 2;
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.XorO);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(17, 127);
+            this.button4.Margin = new System.Windows.Forms.Padding(8);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(49, 42);
+            this.button4.TabIndex = 3;
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.XorO);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(82, 127);
+            this.button5.Margin = new System.Windows.Forms.Padding(8);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(49, 42);
+            this.button5.TabIndex = 4;
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.XorO);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(147, 127);
+            this.button6.Margin = new System.Windows.Forms.Padding(8);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(49, 42);
+            this.button6.TabIndex = 5;
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.XorO);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(147, 185);
+            this.button9.Margin = new System.Windows.Forms.Padding(8);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(49, 42);
+            this.button9.TabIndex = 8;
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.XorO);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(82, 185);
+            this.button8.Margin = new System.Windows.Forms.Padding(8);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(49, 42);
+            this.button8.TabIndex = 7;
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.XorO);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(17, 185);
+            this.button7.Margin = new System.Windows.Forms.Padding(8);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(49, 42);
+            this.button7.TabIndex = 6;
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.XorO);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(93, 33);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "label1";
+            // 
+            // MasterBtn
+            // 
+            this.MasterBtn.Location = new System.Drawing.Point(17, 238);
+            this.MasterBtn.Name = "MasterBtn";
+            this.MasterBtn.Size = new System.Drawing.Size(177, 43);
+            this.MasterBtn.TabIndex = 10;
+            this.MasterBtn.Text = "Start Game";
+            this.MasterBtn.UseVisualStyleBackColor = true;
+            this.MasterBtn.Click += new System.EventHandler(this.MasterBtn_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(313, 229);
+            this.panel1.TabIndex = 11;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(3, 104);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(177, 29);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Start Game first";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(17F, 33F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(328, 293);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.MasterBtn);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(8);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button MasterBtn;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,325 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace XO
+{
+    public partial class Form1 : Form
+    {
+        bool IsGameOn = false;
+        int[,] XOs = new int[3,3];
+        bool turaX = false;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            XOs =new int[3,3] { 
+                { 3, 3,3 }, 
+                { 3, 3,3 }, 
+                { 3, 3,3 } };
+        }
+
+        private void XorO(object sender, EventArgs e)
+        {
+            if (IsGameOn)
+            {
+
+
+                Button btn = (Button)sender;
+                if (turaX)
+                {
+                    if (btn.Text == "")
+                    {
+                        switch (btn.TabIndex)
+                        {
+                            case 0:
+                                XOs[0, 0] = 1;
+                                Refresh();
+                                break;
+
+                            case 1:
+                                XOs[0, 1] = 1;
+                                Refresh();
+                                break;
+
+                            case 2:
+                                XOs[0, 2] = 1;
+                                Refresh();
+                                break;
+                            ///////////
+                            case 3:
+                                XOs[1, 0] = 1;
+                                Refresh();
+                                break;
+
+                            case 4:
+                                XOs[1, 1] = 1;
+                                Refresh();
+                                break;
+
+                            case 5:
+                                XOs[1, 2] = 1;
+                                Refresh();
+                                break;
+                            /////////////
+                            case 6:
+                                XOs[2, 0] = 1;
+                                Refresh();
+                                break;
+
+                            case 7:
+                                XOs[2, 1] = 1;
+                                Refresh();
+                                break;
+
+                            case 8:
+                                XOs[2, 2] = 1;
+                                Refresh();
+                                break;
+                            default:
+                                break;
+                        }
+
+                    }
+                }
+                else
+                {
+                    if (btn.Text == "")
+                    {
+                        switch (btn.TabIndex)
+                        {
+                            case 0:
+                                XOs[0, 0] = 2;
+                                Refresh();
+                                break;
+
+                            case 1:
+                                XOs[0, 1] = 2;
+                                Refresh();
+                                break;
+
+                            case 2:
+                                XOs[0, 2] = 2;
+                                Refresh();
+                                break;
+                            ///////////
+                            case 3:
+                                XOs[1, 0] = 2;
+                                Refresh();
+                                break;
+
+                            case 4:
+                                XOs[1, 1] = 2;
+                                Refresh();
+                                break;
+
+                            case 5:
+                                XOs[1, 2] = 2;
+                                Refresh();
+                                break;
+                            /////////////
+                            case 6:
+                                XOs[2, 0] = 2;
+                                Refresh();
+                                break;
+
+                            case 7:
+                                XOs[2, 1] = 2;
+                                Refresh();
+                                break;
+
+                            case 8:
+                                XOs[2, 2] = 2;
+                                Refresh();
+                                break;
+                            default:
+                                break;
+                        }
+
+                    }
+                }
+                ///////////////////
+            }
+        }
+    
+        public void Refresh()
+        {
+            if (XOs[0, 0] == 1)
+            { button1.Text = "X"; } else if (XOs[0,0] == 2) { button1.Text = "O"; };
+
+            if (XOs[0, 1] == 1)
+            { button2.Text = "X"; }
+            else if (XOs[0, 1] == 2) { button2.Text = "O"; };
+
+            if (XOs[0, 2] == 1)
+            { button3.Text = "X"; }
+            else if (XOs[0, 2] == 2) { button3.Text = "O"; };
+
+            ////////////////
+
+            if (XOs[1, 0] == 1)
+            { button4.Text = "X"; }
+            else if (XOs[1, 0] == 2) { button4.Text = "O"; };
+
+            if (XOs[1, 1] == 1)
+            { button5.Text = "X"; }
+            else if (XOs[1, 1] == 2) { button5.Text = "O"; };
+
+            if (XOs[1, 2] == 1)
+            { button6.Text = "X"; }
+            else if (XOs[1, 2] == 2) { button6.Text = "O"; };
+
+            ////////////
+
+            if (XOs[2, 0] == 1)
+            { button7.Text = "X"; }
+            else if (XOs[2, 0] == 2) { button7.Text = "O"; };
+
+            if (XOs[2, 1] == 1)
+            { button8.Text = "X"; }
+            else if (XOs[2, 1] == 2) { button8.Text = "O"; };
+
+            if (XOs[2, 2] == 1)
+            { button9.Text = "X"; }
+            else if (XOs[2, 2] == 2) { button9.Text = "O"; };
+
+            if (turaX)
+            {
+                turaX = false;
+            }
+            else { turaX = true; }
+            Check();
+        }
+
+        public void Check()
+        {
+            if (IsGameOn)
+            {
+                for (int i = 0; i < 3; i++)
+                {
+
+                    if (XOs[i, 0] == XOs[i, 1] && XOs[i, 1] == XOs[i, 2])
+                    {
+                        if (XOs[i, 0] == 3)
+                        {
+
+                        }
+                        else
+                        {
+                            MessageBox.Show("--" + i.ToString());
+                            IsGameOn = false;
+                            EndGame(XOs[i, 0]);
+                        }
+                    }
+
+                    if (XOs[0, i] == XOs[1, i] && XOs[1, i] == XOs[2, i])
+                    {
+                        if (XOs[0, i] != 3)
+                        {
+                            MessageBox.Show(" | " + i.ToString());
+                            IsGameOn = false;
+                            EndGame(XOs[1, i]);
+                        }
+                    }
+
+                }
+
+                if (XOs[0, 2] == XOs[1, 1] && XOs[1, 1] == XOs[2, 0])
+                {
+                    if (XOs[1, 1] != 3)
+                    {
+                        MessageBox.Show("/");
+                        IsGameOn = false;
+                        EndGame(XOs[1, 1]);
+                    }
+                }
+
+                if (XOs[0, 0] == XOs[1, 1] && XOs[1, 1] == XOs[2, 2])
+                {
+                    if (XOs[1, 1] != 3)
+                    {
+                        MessageBox.Show(@"\");
+                        IsGameOn = false;
+                        EndGame(XOs[1, 1]);
+                    }
+                }
+            }
+            else
+            {//Reset game
+                XOs = new int[3, 3] {
+                { 3, 3,3 },
+                { 3, 3,3 },
+                { 3, 3,3 } };
+                Controls.OfType<Button>().ToList().ForEach(button => { button.Text = ""; });
+            }
+
+            if (!XOs.Cast<int>().ToList().Contains(3))
+            {
+                //Game should be finished by now
+                MessageBox.Show("You stuck ?");
+                label1.Text = "Finished";
+            }
+            
+        }
+
+        private void MasterBtn_Click(object sender, EventArgs e)
+        {
+            if (!IsGameOn) //Check for running game
+            {
+                label1.Text = "Game is on";
+                panel1.Hide();
+                label2.Hide();
+                IsGameOn = true;
+            }
+            else
+            {
+                EndGame(666);
+                //panel1.Show();
+                //label2.Text = "Thanks for Playing";
+                //label2.Show();
+            }
+        }
+
+        void EndGame(int GameState)
+        {
+            label1.Text = "Finished";
+            switch (GameState)
+            {
+                case 1:
+                    //MessageBox.Show("X Wins");
+                    label2.Text = "X Wins";
+                    break;
+
+                case 2:
+                    //MessageBox.Show("O Wins");
+                    label2.Text = "O Wins";
+                    break;
+
+                case 3:
+                    MessageBox.Show("Err St:3");
+                    break;
+
+                default:
+                    MessageBox.Show("Game Brokie ?");
+                    break;
+            }
+            panel1.Show();
+            label2.Show();
+            XOs = new int[3, 3] {
+                { 3, 3,3 },
+                { 3, 3,3 },
+                { 3, 3,3 } };
+            Controls.OfType<Button>().ToList().ForEach(button => { button.Text = ""; });
+            MasterBtn.Text = "Restart";
+            IsGameOn= false;
+        }
+    }
+}
